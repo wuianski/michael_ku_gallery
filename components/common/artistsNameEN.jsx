ef883ffa99124ba291eb646f439e106dd30fe31f
wuianski <wuiyeh@gmail.com>
2025-02-25 18:45:11 +0800

    update the link of artist's name

--- components/common/artistsNameEN.jsx
+++ components/common/artistsNameEN.jsx
@@ -7,13 +7,13 @@ export default function ArtistsNameEN({ artists }) {
       {artists.map((a, index) => (
         <Box component="span" key={index}>
           {index === artists.length - 1 ? (
-            <Link href={`/artists/${a.artists_id.id}`} passHref>
+            <Link href={`/artists/${a.artists_id.name_tw}`} passHref>
               <Box mr={0.5} component="span" key={index} className="en_font">
                 {a.artists_id.name_en}
               </Box>
             </Link>
           ) : (
-            <Link href={`/artists/${a.artists_id.id}`} passHref>
+            <Link href={`/artists/${a.artists_id.name_tw}`} passHref>
               <Box mr={0.5} component="span" key={index} className="en_font">
                 {a.artists_id.name_en} /
               </Box>
--- components/common/artistsNameTW.jsx
+++ components/common/artistsNameTW.jsx
@@ -2,19 +2,19 @@ import { Box } from "@mui/material";
 import Link from "next/link";
 
 export default function ArtistsNameTW({ artists }) {
-  // console.log(artists);
+  console.log(artists);
   return (
     <>
       {artists.map((a, index) => (
         <Box component="span" key={index}>
           {index === artists.length - 1 ? (
-            <Link href={`/artists/${a.artists_id.id}`} passHref>
+            <Link href={`/artists/${a.artists_id.name_tw}`} passHref>
               <Box mr={0.5} component="span" key={index} className="tw_font">
                 {a.artists_id.name_tw}
               </Box>
             </Link>
           ) : (
-            <Link href={`/artists/${a.artists_id.id}`} passHref>
+            <Link href={`/artists/${a.artists_id.name_tw}`} passHref>
               <Box mr={0.5} component="span" key={index} className="tw_font">
                 {a.artists_id.name_tw} /
               </Box>
